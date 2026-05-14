46b6ea5eae55461dd04dbd5266614d4df8634357
Andres <atm1996.ast@gmail.com>
2026-05-14 00:19:13 -0600

    cambios para pruebas

--- src/test/java/com/security/practice/users/UsersApplicationTests.java
+++ src/test/java/com/security/practice/users/UsersApplicationTests.java
@@ -4,7 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+
+@SpringBootTest(properties = {
+		"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"
+})
 @ActiveProfiles("test")
 class UsersApplicationTests {
 
